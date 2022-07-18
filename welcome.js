a5b7e40e3bb76fdb6987f7a6aef6ea646f9f5e8a
Tereza Dankova <104557274+TerezaDankova@users.noreply.github.com>
2022-07-18 16:39:07 +0200

    update js

--- welcome.js
+++ welcome.js
@@ -1,14 +1,14 @@
 window.open ("./welcome.html","_self");
 
-const checkbox =document.querySelector("#checkbox")
+const checkbox = document.querySelector("#checkbox")
 checkbox.addEventListener("click", function () {
   checkbox.classList.toggle("selected")
 })
 
 function proceedButton () {
   if(checkbox.classList.contains("selected")) { 
-     window.location.href = "index2.html"; } 
-     else {
+     window.location.href = "questions.html"; 
+  } else {
      alert("For proceed to another page you need to check the box."); 
      }
 }
