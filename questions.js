f3afd618e8572b7cb4d01f7c55fd9e9df15808cb
Pasha127 <pasha.account@gmail.com>
2022-07-21 10:40:59 +0200

    removed comment

--- questions.js
+++ questions.js
@@ -256,8 +256,7 @@ myChart.options.cutout =  35
         sessionStorage.setItem("score", pointCount);
         window.open("./results.html","_self");
         let newScoreArray = JSON.parse(localStorage.getItem("data"));           
-        newScoreArray[0][0]=pointCount.toString();  
-        //newScoreArray[0].splice(1,0, "points");  <------ this adds "points" twice but I don't know why   
+        newScoreArray[0][0]=pointCount.toString();          
         console.log(newScoreArray[0]);
         localStorage.setItem("data", JSON.stringify(newScoreArray));
 
