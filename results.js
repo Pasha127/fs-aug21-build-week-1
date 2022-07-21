6515fc6a2cba604c0ecf03d0576e781ad4699396
sas888 <syli888@outlook.com>
2022-07-21 20:11:47 +0100

    update

--- results.js
+++ results.js
@@ -6,17 +6,16 @@ const wrong = total - correct;
 let newScoresArray = JSON.parse(localStorage.getItem("data")); //to read
 console.log(newScoresArray);
 
-for(let i = 0; i<newScoresArray.length; i++){
-  if(newScoresArray[i][0]<10){
-    newScoresArray[i] = "0"+newScoresArray[i].toString()
-    newScoresArray[i] = newScoresArray[i].replaceAll(',',' ');
-  }else{
-    newScoresArray[i] = newScoresArray[i].toString().replaceAll(',',' ');    
+for (let i = 0; i < newScoresArray.length; i++) {
+  if (newScoresArray[i][0] < 10) {
+    newScoresArray[i] = "0" + newScoresArray[i].toString();
+    newScoresArray[i] = newScoresArray[i].replaceAll(",", " ");
+  } else {
+    newScoresArray[i] = newScoresArray[i].toString().replaceAll(",", " ");
   }
 }
 let arrayOfScoreDivs = newScoresArray.sort().reverse();
 //console.log(arrayOfScoreDivs);
-
 
 const leaderBoard = document.querySelector("#leaderBoardDiv");
 const correctPercentage = (correct / total) * 100;
@@ -70,23 +69,20 @@ btnRateUs.addEventListener(
   () => (btnRateUs.style.backgroundColor = "#00FFFF")
 );
 
-
-let arrayGenerator = function(){   
-  
-  for(let i=0; i<arrayOfScoreDivs.length; i++){
+let arrayGenerator = function () {
+  for (let i = 0; i < arrayOfScoreDivs.length; i++) {
     const newScoreDiv = document.createElement("div");
     newScoreDiv.classList.add("scoreDiv");
-    const newScoreDivText = document.createElement('p');
+    const newScoreDivText = document.createElement("p");
     newScoreDivText.innerText = arrayOfScoreDivs[i];
     leaderBoard.append(newScoreDiv);
-    newScoreDiv.append(newScoreDivText); 
+    newScoreDiv.append(newScoreDivText);
     //console.log(arrayOfScoreDivs)
   }
-
-}
+};
 arrayGenerator();
 
-const clearBoard = function (){
+const clearBoard = function () {
   localStorage.clear();
   location.reload();
-}
+};
